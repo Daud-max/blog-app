e6c3f35a42494804b95087877a32abff56d0741d
AHMAD SAAD <dn32206@gmail.com>
2024-12-23 04:29:38 +0500

    hero section for home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import React from "react";
+import Hero from "../Home/Hero";
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <Hero />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
