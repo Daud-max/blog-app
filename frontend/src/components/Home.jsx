e046e91cb7cec559c3257b3900b03c71a21d1abf
AHMAD SAAD <dn32206@gmail.com>
2024-12-23 05:01:32 +0500

    creator component of home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "../Home/Hero";
 import Trending from "../Home/Trending";
 import Devotional from "../Home/Devotional";
+import Creator from "../Home/Creators";
 function Home() {
   return (
     <div>
       <Hero />
       <Trending />
       <Devotional />
+      <Creator />
     </div>
   );
 }
